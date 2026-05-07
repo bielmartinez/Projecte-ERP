68d5ec8ee8938362b0fe7ad8a1e618d4a416cfcc
Biel <bielmartinezcaceres16@gmail.com>
2026-05-08 01:05:54 +0200

    Canvi Cors desplegament v2

--- backend/app/Config/Cors.php
+++ backend/app/Config/Cors.php
@@ -38,6 +38,7 @@ class Cors extends BaseConfig
             'http://localhost:5173',
             'http://127.0.0.1:5173',
             'frontend-production-65db.up.railway.app',
+            'https://frontend-production-65db.up.railway.app/',
         ],
 
         /**
